fde8ea8d7e0ffdcaa6bf832fbf6201e6df59024f
MsStacy08 <103374414+MsStacy08@users.noreply.github.com>
2020-12-02 12:23:21 +0000

    netfilter: IDLETIMER: fix invalid deference of timer

--- addData2Sqlite.js
+++ addData2Sqlite.js
@@ -8,4 +8,4 @@ const sqlite3 = require('sqlite3').verbose();
 var res = ['mylog.txt','03.10.2018','m1','Area1','ok','Load']
 let db;
 // exist database
-fs.exists('./event.db', function
+fs.exists('./event.db', function(
