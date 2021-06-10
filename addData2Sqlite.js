79cf274f1e71ea688090bc6fd26ef121c2ab7a3f
MsStacy08 <103374414+MsStacy08@users.noreply.github.com>
2021-06-10 10:41:46 +0000

    adding files

--- addData2Sqlite.js
+++ addData2Sqlite.js
@@ -17,4 +17,4 @@ fs.exists('./event.db', function(exists) {
   } else {
     // database is not existing
     console.log('open database and create table');
-    db = new sqlite3.Database('./eve
+    db = new sqlite3.Database('./event
