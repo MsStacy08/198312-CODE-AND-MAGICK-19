d7aa7c28fb68f116cc9b14b7b81bf326b525af3f
MsStacy08 <103374414+MsStacy08@users.noreply.github.com>
2022-03-07 22:33:50 +0000

    legacy

--- addData2Sqlite.js
+++ addData2Sqlite.js
@@ -49,4 +49,4 @@ function DoMain(db,res) {
 }
 
 function doAdd2DB(db,entry) {
-db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, 
+db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, a
