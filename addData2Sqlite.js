a88e267583e6101e0afb1e72fea3e8de2500765f
MsStacy08 <103374414+MsStacy08@users.noreply.github.com>
2022-03-15 10:02:28 +0000

    remove redundant line

--- addData2Sqlite.js
+++ addData2Sqlite.js
@@ -49,4 +49,4 @@ function DoMain(db,res) {
 }
 
 function doAdd2DB(db,entry) {
-db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, area, status, ac
+db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, area, status, act
