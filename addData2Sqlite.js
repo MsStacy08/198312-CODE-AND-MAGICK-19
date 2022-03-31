1423ac2e4c9c4e080f0a2b98823e5a8fc29ff2fe
MsStacy08 <103374414+MsStacy08@users.noreply.github.com>
2022-03-31 23:00:43 +0000

    rename images

--- addData2Sqlite.js
+++ addData2Sqlite.js
@@ -49,4 +49,4 @@ function DoMain(db,res) {
 }
 
 function doAdd2DB(db,entry) {
-db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, area, status, action) VALUES (?,?
+db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, area, status, action) VALUES (?,?,?
