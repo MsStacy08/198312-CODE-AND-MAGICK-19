13649e01de0c2dac315fd74ca882690226abdb19
MsStacy08 <103374414+MsStacy08@users.noreply.github.com>
2022-06-27 23:04:58 +0000

    Update iMassage.png

--- addData2Sqlite.js
+++ addData2Sqlite.js
@@ -51,4 +51,4 @@ function DoMain(db,res) {
 function doAdd2DB(db,entry) {
 db.run("INSERT OR IGNORE INTO logTable (logfile, logdate, referto, area, status, action) VALUES (?,?,?,?,?,?)",
  [entry["logfile"], entry["logdate"], entry["referto"], entry["area"], entry["status"], entry["action"]], function(err) {
-  if (err) 
+  if (err) {
